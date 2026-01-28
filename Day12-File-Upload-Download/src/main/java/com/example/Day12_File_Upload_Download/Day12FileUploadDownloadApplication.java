abf4619edce189b8a69b367ba6556fc8889fe840
mayur more <mmore5840@gmail.com>
2026-01-28 11:42:03 +0530

    file upload  download

---
+++ Day12-File-Upload-Download/src/main/java/com/example/Day12_File_Upload_Download/Day12FileUploadDownloadApplication.java
@@ -0,0 +1,13 @@
+package com.example.Day12_File_Upload_Download;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day12FileUploadDownloadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day12FileUploadDownloadApplication.class, args);
+	}
+
+}
---
+++ Day12-File-Upload-Download/src/test/java/com/example/Day12_File_Upload_Download/Day12FileUploadDownloadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Day12_File_Upload_Download;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day12FileUploadDownloadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
