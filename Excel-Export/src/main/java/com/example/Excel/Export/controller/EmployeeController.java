dd7dedbd29eeb2c8cbd29b7dbeb1935f747e2a29
mayur more <mmore5840@gmail.com>
2026-02-03 22:56:06 +0530

    add database

--- Excel-Export/src/main/java/com/example/Excel/Export/controller/EmployeeController.java
+++ Excel-Export/src/main/java/com/example/Excel/Export/controller/EmployeeController.java
@@ -1,5 +1,44 @@
 package com.example.Excel.Export.controller;
 
-public class EmployeeController {
+import java.io.ByteArrayInputStream;
+import java.net.http.HttpHeaders;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.Excel.Export.model.Employee;
+import com.example.Excel.Export.repository.EmployeeRepository;
+import com.example.Excel.Export.service.ExcelService;
+
+@RestController
+@RequestMapping("/employees")
+public class EmployeeController {
+	
+	@Autowired
+	private EmployeeRepository repository;
+	
+	@Autowired
+	private ExcelService service;
+	
+	 @GetMapping("/export")
+	    public ResponseEntity<InputStreamResource> exportExcel() {
+
+	        List<Employee> employees = repository.findAll();
+	        ByteArrayInputStream excel = ExcelService.exportToExcel(employees);
+
+	        HttpHeaders headers = new HttpHeaders(null);
+	        headers.add("Content-Disposition", "attachment; filename=employees.xlsx");
+
+	        return ResponseEntity.ok()
+	                .headers(headers)
+	                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+	                .body(new InputStreamResource(excel));
+	    }
 }
