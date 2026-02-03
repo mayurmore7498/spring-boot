9da8cd0b2351bfc04ca081b32ba8af358b81de6e
mayur more <mmore5840@gmail.com>
2026-02-03 22:25:04 +0530

    add entity

---
+++ Excel-Export/src/main/java/com/example/Excel/Export/model/Employee.java
@@ -0,0 +1,42 @@
+package com.example.Excel.Export.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Employee {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private long id;
+	
+	private String name;
+	private String Department;
+	private double salary;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDepartment() {
+		return Department;
+	}
+	public void setDepartment(String department) {
+		Department = department;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	
+	
+
+}
